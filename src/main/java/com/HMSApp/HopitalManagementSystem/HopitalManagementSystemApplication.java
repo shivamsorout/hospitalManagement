71e8c228377d456e877b6a7d8db1ec77f079120e
Shivam <shivam@e-labs.ai>
2024-07-11 14:39:18 +0530

    new project

---
+++ src/main/java/com/HMSApp/HopitalManagementSystem/HopitalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.HMSApp.HopitalManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopitalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopitalManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HMSApp/HopitalManagementSystem/HopitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HMSApp.HopitalManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
